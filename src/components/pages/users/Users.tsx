5ffaaf473f1977cf802767d1972188d686f90578
Bayrem 98 <bayremsayeh98@gmail.com>
2023-06-16 19:11:28 +0200

    changed many style for users page

--- src/components/pages/users/Users.tsx
+++ src/components/pages/users/Users.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ButtonGroup, Container, Table } from "reactstrap";
+import { ButtonGroup, Table } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBoxOpen } from "@fortawesome/free-solid-svg-icons";
 import User from "../../../@types/User";
@@ -26,65 +26,63 @@ const UsersTable = (props: Props) => {
         <Inscription refresh={() => getUsers(setUsers)} />
       </div>
       <br />
-      <Table bordered responsive hover>
-        <thead>
-          <tr>
-            <th
-              className="font-['Helvetica']"
-              style={{ color: "#0e0e0ee7", backgroundColor: "lightgray" }}
-            >
-              Username
-            </th>
-            <th
-              className="w-[90px] font-['Helvetica']"
-              style={{
-                color: "#0e0e0ee7",
-                backgroundColor: "lightgray",
-                textAlign: "center",
-              }}
-            >
-              Action
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.length ? (
-            users.map((user) => (
-              <tr key={user._id}>
-                <td
-                  style={{
-                    color: "#0e0e0ee7",
-                    fontSize: 18,
-                  }}
-                >
-                  {user.username}
-                </td>
+      <div style={{ paddingLeft: 200, paddingRight: 200 }}>
+        <Table bordered responsive hover>
+          <thead>
+            <tr>
+              <th style={{ color: "#0e0e0ee7", backgroundColor: "lightgray" }}>
+                Username
+              </th>
+              <th
+                style={{
+                  color: "#0e0e0ee7",
+                  backgroundColor: "lightgray",
+                  textAlign: "center",
+                }}
+              >
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length ? (
+              users.map((user) => (
+                <tr key={user._id}>
+                  <td
+                    style={{
+                      color: "#0e0e0ee7",
+                      fontSize: 18,
+                    }}
+                  >
+                    {user.username}
+                  </td>
 
-                <td style={{ textAlign: "center" }}>
-                  <ButtonGroup>
-                    <UserDelete
-                      user={user}
-                      refresh={() => getUsers(setUsers)}
-                    />
-                  </ButtonGroup>
+                  <td style={{ textAlign: "center" }}>
+                    <ButtonGroup>
+                      <UserDelete
+                        user={user}
+                        refresh={() => getUsers(setUsers)}
+                      />
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td
+                  colSpan={4}
+                  className="text-center p-5"
+                  style={{ color: "#0e0e0ee7" }}
+                >
+                  <FontAwesomeIcon icon={faBoxOpen} size="4x" />
+                  <br />
+                  Pas de données...
                 </td>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td
-                colSpan={4}
-                className="text-center p-5"
-                style={{ color: "#0e0e0ee7" }}
-              >
-                <FontAwesomeIcon icon={faBoxOpen} size="4x" />
-                <br />
-                Pas de données...
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
+            )}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 };
